3ad91a95b24a99a758329abb618711ab1d3eefd0
Marco <mbortoli90@gmail.com>
2019-08-12 21:22:19 +0200

    Groups description/animations

---
+++ animated/css/java.js
@@ -0,0 +1,231 @@
+// JavaScript Document
+ $(document).ready(function(){
+
+
+
+  $(".container_doublewidth_nonmetals").hover(
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid white");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid white");
+           $(".container_noble").css("border", "0.3vh solid white");
+           $(".container_metalloids").css("border", "0.3vh solid white");
+           $(".container_transition").css("border", "0.3vh solid white");
+           $(".container_posttransition").css("border", "0.3vh solid white");
+           $(".container_actinides").css("border", "0.3vh solid white");
+           $(".container_lanthanides").css("border", "0.3vh solid white");
+  },
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid orange");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid yellow");
+           $(".container_noble").css("border", "0.3vh solid deepskyblue");
+           $(".container_transition").css("border", "0.3vh solid pink");
+           $(".container_posttransition").css("border", "0.3vh solid darkturquoise");
+           $(".container_actinides").css("border", "0.3vh solid chocolate");
+           $(".container_lanthanides").css("border", "0.3vh solid violet");
+           $(".container_metalloids").css("border", "0.3vh solid seagreen");
+
+  }
+
+  );
+    
+$(".container_doublewidth_alkali").hover(
+  function(){
+           $(".container_nonmetals").css("border", "0.3vh solid white");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid white");
+           $(".container_noble").css("border", "0.3vh solid white");
+           $(".container_metalloids").css("border", "0.3vh solid white");
+           $(".container_transition").css("border", "0.3vh solid white");
+           $(".container_posttransition").css("border", "0.3vh solid white");
+           $(".container_actinides").css("border", "0.3vh solid white");
+           $(".container_lanthanides").css("border", "0.3vh solid white");
+  },
+  function(){
+           $(".container_nonmetals").css("border", "0.3vh solid lime");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid yellow");
+           $(".container_noble").css("border", "0.3vh solid deepskyblue");
+           $(".container_transition").css("border", "0.3vh solid pink");
+           $(".container_posttransition").css("border", "0.3vh solid darkturquoise");
+           $(".container_actinides").css("border", "0.3vh solid chocolate");
+           $(".container_lanthanides").css("border", "0.3vh solid violet");
+           $(".container_metalloids").css("border", "0.3vh solid seagreen");
+
+  }
+
+  );
+
+$(".container_doublewidth_earth_alkali").hover(
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid white");		 
+           $(".container_nonmetals").css("border", "0.3vh solid white");
+           $(".container_noble").css("border", "0.3vh solid white");
+           $(".container_metalloids").css("border", "0.3vh solid white");
+           $(".container_transition").css("border", "0.3vh solid white");
+           $(".container_posttransition").css("border", "0.3vh solid white");
+           $(".container_actinides").css("border", "0.3vh solid white");
+           $(".container_lanthanides").css("border", "0.3vh solid white");
+  },
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid orange");		 
+           $(".container_nonmetals").css("border", "0.3vh solid lime");
+           $(".container_noble").css("border", "0.3vh solid deepskyblue");
+           $(".container_transition").css("border", "0.3vh solid pink");
+           $(".container_posttransition").css("border", "0.3vh solid darkturquoise");
+           $(".container_actinides").css("border", "0.3vh solid chocolate");
+           $(".container_lanthanides").css("border", "0.3vh solid violet");
+           $(".container_metalloids").css("border", "0.3vh solid seagreen");
+
+  }
+
+  );
+
+$(".container_doublewidth_transition").hover(
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid white");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid white");
+           $(".container_noble").css("border", "0.3vh solid white");
+           $(".container_metalloids").css("border", "0.3vh solid white");
+           $(".container_nonmetals").css("border", "0.3vh solid white");
+           $(".container_posttransition").css("border", "0.3vh solid white");
+           $(".container_actinides").css("border", "0.3vh solid white");
+           $(".container_lanthanides").css("border", "0.3vh solid white");
+  },
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid orange");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid yellow");
+           $(".container_noble").css("border", "0.3vh solid deepskyblue");
+           $(".container_nonmetals").css("border", "0.3vh solid lime");
+           $(".container_posttransition").css("border", "0.3vh solid darkturquoise");
+           $(".container_actinides").css("border", "0.3vh solid chocolate");
+           $(".container_lanthanides").css("border", "0.3vh solid violet");
+           $(".container_metalloids").css("border", "0.3vh solid seagreen");
+
+  }
+
+  );
+ 
+$(".container_doublewidth_lanthanides").hover(
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid white");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid white");
+           $(".container_noble").css("border", "0.3vh solid white");
+           $(".container_metalloids").css("border", "0.3vh solid white");
+           $(".container_transition").css("border", "0.3vh solid white");
+           $(".container_posttransition").css("border", "0.3vh solid white");
+           $(".container_actinides").css("border", "0.3vh solid white");
+           $(".container_nonmetals").css("border", "0.3vh solid white");
+  },
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid orange");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid yellow");
+           $(".container_noble").css("border", "0.3vh solid deepskyblue");
+           $(".container_transition").css("border", "0.3vh solid pink");
+           $(".container_posttransition").css("border", "0.3vh solid darkturquoise");
+           $(".container_actinides").css("border", "0.3vh solid chocolate");
+           $(".container_nonmetals").css("border", "0.3vh solid lime");
+           $(".container_metalloids").css("border", "0.3vh solid seagreen");
+
+  }
+
+  );
+
+$(".container_doublewidth_metalloids").hover(
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid white");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid white");
+           $(".container_noble").css("border", "0.3vh solid white");
+           $(".container_nonmetals").css("border", "0.3vh solid white");
+           $(".container_transition").css("border", "0.3vh solid white");
+           $(".container_posttransition").css("border", "0.3vh solid white");
+           $(".container_actinides").css("border", "0.3vh solid white");
+           $(".container_lanthanides").css("border", "0.3vh solid white");
+  },
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid orange");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid yellow");
+           $(".container_noble").css("border", "0.3vh solid deepskyblue");
+           $(".container_transition").css("border", "0.3vh solid pink");
+           $(".container_posttransition").css("border", "0.3vh solid darkturquoise");
+           $(".container_actinides").css("border", "0.3vh solid chocolate");
+           $(".container_lanthanides").css("border", "0.3vh solid violet");
+           $(".container_nonmetals").css("border", "0.3vh solid lime");
+
+  }
+
+  );
+
+$(".container_doublewidth_noble").hover(
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid white");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid white");
+           $(".container_nonmetals").css("border", "0.3vh solid white");
+           $(".container_metalloids").css("border", "0.3vh solid white");
+           $(".container_transition").css("border", "0.3vh solid white");
+           $(".container_posttransition").css("border", "0.3vh solid white");
+           $(".container_actinides").css("border", "0.3vh solid white");
+           $(".container_lanthanides").css("border", "0.3vh solid white");
+  },
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid orange");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid yellow");
+           $(".container_nonmetals").css("border", "0.3vh solid lime");
+           $(".container_transition").css("border", "0.3vh solid pink");
+           $(".container_posttransition").css("border", "0.3vh solid darkturquoise");
+           $(".container_actinides").css("border", "0.3vh solid chocolate");
+           $(".container_lanthanides").css("border", "0.3vh solid violet");
+           $(".container_metalloids").css("border", "0.3vh solid seagreen");
+
+  }
+
+  );
+
+$(".container_doublewidth_posttransition").hover(
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid white");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid white");
+           $(".container_noble").css("border", "0.3vh solid white");
+           $(".container_metalloids").css("border", "0.3vh solid white");
+           $(".container_transition").css("border", "0.3vh solid white");
+           $(".container_nonmetals").css("border", "0.3vh solid white");
+           $(".container_actinides").css("border", "0.3vh solid white");
+           $(".container_lanthanides").css("border", "0.3vh solid white");
+  },
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid orange");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid yellow");
+           $(".container_noble").css("border", "0.3vh solid deepskyblue");
+           $(".container_transition").css("border", "0.3vh solid pink");
+           $(".container_nonmetals").css("border", "0.3vh solid lime");
+           $(".container_actinides").css("border", "0.3vh solid chocolate");
+           $(".container_lanthanides").css("border", "0.3vh solid violet");
+           $(".container_metalloids").css("border", "0.3vh solid seagreen");
+
+  }
+
+  );
+ 
+$(".container_doublewidth_actinides").hover(
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid white");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid white");
+           $(".container_noble").css("border", "0.3vh solid white");
+           $(".container_metalloids").css("border", "0.3vh solid white");
+           $(".container_transition").css("border", "0.3vh solid white");
+           $(".container_posttransition").css("border", "0.3vh solid white");
+           $(".container_nonmetals").css("border", "0.3vh solid white");
+           $(".container_lanthanides").css("border", "0.3vh solid white");
+  },
+  function(){
+           $(".container_alkali").css("border", "0.3vh solid orange");		 
+           $(".container_earth_alkali").css("border", "0.3vh solid yellow");
+           $(".container_noble").css("border", "0.3vh solid deepskyblue");
+           $(".container_transition").css("border", "0.3vh solid pink");
+           $(".container_posttransition").css("border", "0.3vh solid darkturquoise");
+           $(".container_nonmetals").css("border", "0.3vh solid lime");
+           $(".container_lanthanides").css("border", "0.3vh solid violet");
+           $(".container_metalloids").css("border", "0.3vh solid seagreen");
+
+  }
+
+  );
+
+});
